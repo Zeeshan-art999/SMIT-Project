88aa72743830f88b5d8721eb6dc443ef16e6e48e
Zeeshan <baog83600@gmail.com>
2026-02-10 13:28:20 -0800

    JS CH 21-25

--- JS Chapters/JS CH 21-25/script.js
+++ JS Chapters/JS CH 21-25/script.js
@@ -1,16 +1,24 @@
-    var originalText = "I love my country Pakistan.<br>I like my city Faisalabad.<br>I love my HomeLand."
+document.getElementById("clearInput").onclick=function(){
+    document.getElementById("inputText").value="";
+}
 
+document.getElementById("clearOutput").onclick=function(){
+    document.getElementById("output").innerHTML="";
+}
+
+
+var originalText = "I love my country Pakistan.<br>I like my city Faisalabad.<br>I love my HomeLand."
 
 function lowerCase() {
     // var originalText="Zeeshan Sarfraz";
-    // var originalText = document.getElementById("inputText").value;
+    var originalText = document.getElementById("inputText").value;
     let lowerCaseText = originalText.toLowerCase();
     document.getElementById("output").innerHTML = lowerCaseText;
     // showOutput(lowerCaseText)
 }
 
 function upperCase() {
-    // var originalText = document.getElementById("inputText").value;
+    var originalText = document.getElementById("inputText").value;
     let lowerCaseText = originalText.toUpperCase();
     document.getElementById("output").innerHTML = lowerCaseText;
     // showOutput(upperCaseText)
@@ -23,7 +31,7 @@ function capitalize() {
 
 function betterFormatting() {
     // let text=inputValue();
-    // let originalText = document.getElementById("inputText").value;
+    let originalText = document.getElementById("inputText").value;
     text = originalText.toLowerCase();
     document.getElementById("output").style.textTransform = "capitalize";
     document.getElementById("output").innerHTML = text;
@@ -89,4 +97,55 @@ function checkCityInList() {
         let html = '<span style="color:green;font-size:20px">"' + cityWordInCapitalize + '"</span> found in the list.';
         document.getElementById("output").innerHTML = html
     }
+}
+
+function findWord() {
+    let newOriginalText = originalText.toLowerCase();
+    let word = document.getElementById("inputText").value;
+    if (!word) {
+        alert("Please type a word for find.");
+        return;
+    }
+    word = word.toLowerCase();
+    let findWord = newOriginalText.indexOf(word);
+    // let findWord = newOriginalText.lastIndexOf(word);
+
+    if (findWord !== -1) {
+        let html = 'Your word <span style="color:green;font-size: 18px">"' + word + '"</span> found at index:' + findWord;
+        document.getElementById("output").innerHTML = html
+    } else {
+        let html = 'Your word <span style="color:red;font-size: 18px">"' + word + '"</span> does not exist in the Original String';
+        document.getElementById("output").innerHTML = html
+    }
+}
+
+function replaceWord() {
+    // let cinic = "33102-270194-45"
+    // if(cinic.includes("-") || cinic.includes(" ") || cinic.includes(".") ){
+    //     alert("please enter yout cinic without dashes dot or spaces .")
+    //     return;
+    // }
+    // let word = "-";
+    // let replaceWith = "";
+    // word = new RegExp(word,'g');
+    // let cinicWithoutDashs = cinic.replaceAll(word,"");
+    // console.log(cinicWithoutDashs)
+
+    let newOriginalText = originalText.toLowerCase();
+    let word = document.getElementById("inputText").value;
+    if (!word) {
+        alert("Please type a word for replacing.");
+        return;
+    }
+
+    let replaceWith = prompt("Enter a word that you want to replace with");
+    if (!replaceWith) {
+        alert('Please type a word to replace it with' + word + ".")
+        return;
+    }
+    word = word.toLowerCase();
+    word = new RegExp(word, 'g');
+    replaceWith = replaceWith.toLowerCase();
+    let replaceWord = newOriginalText.replace(word, replaceWith);
+    document.getElementById("output").innerHTML = replaceWord;
 }
